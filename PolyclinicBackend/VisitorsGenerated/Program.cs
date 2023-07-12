dc5e04936cdf7c6caa565a789f570d7e03097993
Maxim Sikanov <max.4251474@yandex.ru>
2023-07-12 16:54:12 +0300

    [DS-19]-fix-generate

--- PolyclinicBackend/VisitorsGenerated/Program.cs
+++ PolyclinicBackend/VisitorsGenerated/Program.cs
@@ -1,35 +1,32 @@
-﻿using System;
-using System.Linq;
-using Bogus;
+﻿using Bogus;
 using Data.DAL.Context;
 using Data.DAL.Entities;
 using Microsoft.EntityFrameworkCore;
+
 
 using (var dbContext = new GeneratedContext())
 {
     // Удаление существующих записей из таблицы
     dbContext.Database.ExecuteSqlRaw("TRUNCATE TABLE public.\"VisitorGenerated\" RESTART IDENTITY");
-
+    List<string> patronymics = File.ReadAllLines("C:\\Users\\max42\\OneDrive\\Рабочий стол\\test\\analytics\\rus_midname.txt").ToList();
     // Генерация случайных данных
     var faker = new Faker<Visitor>("ru")
         .RuleFor(p => p.FirstName, f => f.Name.FirstName())
         .RuleFor(p => p.LastName, f => f.Name.LastName())
-        .RuleFor(p => p.FatherName, f => f.Name.FirstName())
+        .RuleFor(p => p.FatherName, f => f.PickRandom(patronymics))
         .RuleFor(p => p.City, f => f.Address.City())
-        .RuleFor(p => p.Gender, f => f.PickRandom("Male", "Female"))
-        .RuleFor(p => p.BirthDate, f => f.Date.Past(30, DateTime.Now.AddYears(-18)).ToUniversalTime())
-        .RuleFor(p => p.Nationality, f => f.Address.Country())
+        .RuleFor(p => p.Gender, f => f.PickRandom("M", "F"))
+        .RuleFor(p => p.BirthDate, f => f.Date.Past(30, DateTime.Now.AddYears(-18)).ToUniversalTime().Date)
+        .RuleFor(p => p.Nationality, "RU")
         .RuleFor(p => p.PassportSeries, f => f.Random.Number(1000, 9999))
         .RuleFor(p => p.PassportNumber, f => f.Random.Number(100000, 999999))
         .RuleFor(p => p.PhotoBase64, f => Convert.ToBase64String(f.Random.Bytes(10)))
-        .RuleFor(p => p.DateIssue, f => f.Date.Past(5).ToUniversalTime());
+        .RuleFor(p => p.DateIssue, f => f.Date.Past(5).ToUniversalTime().Date);
 
     // Генерация 100 записей и добавление их в базу данных
-    var visitors = faker.Generate(100);
+    var visitors = faker.Generate(1000);
     dbContext.VisitorGenerated.AddRange(visitors);
     dbContext.SaveChanges();
 
     Console.WriteLine("Данные успешно добавлены в базу данных.");
 }
-
-Console.ReadLine();
