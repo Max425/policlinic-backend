c0a688ec89e5da61f80b555e3ff70ee814ddf2a8
Maxim Sikanov <max.4251474@yandex.ru>
2023-07-21 12:11:28 +0300

    [DS-15]-generate-db

--- PolyclinicBackend/Test/Program.cs
+++ PolyclinicBackend/Test/Program.cs
@@ -1,38 +1,96 @@
-﻿/*using Bogus;
+﻿using Bogus;
 using Data.DAL.Context;
 using Data.DAL.Entities;
 
+namespace Test;
 
-using (var dbContext = new GeneratedContext())
+public class Program
 {
-    // Удаление существующих записей из таблицы
-    //dbContext.Database.ExecuteSqlRaw("TRUNCATE TABLE public.\"VisitorGenerated\" RESTART IDENTITY");
-    List<string> patronymics = File.ReadAllLines("..\\..\\..\\rus_midname.txt").ToList();
-    // Генерация случайных данных
-    var faker = new Faker<Visitor>("ru")
-        .RuleFor(p => p.FirstName, f => f.Name.FirstName())
-        .RuleFor(p => p.LastName, f => f.Name.LastName())
-        .RuleFor(p => p.FatherName, f => f.PickRandom(patronymics))
-        .RuleFor(p => p.City, f => f.Address.City())
-        .RuleFor(p => p.Gender, f => f.PickRandom("M", "F"))
-        .RuleFor(p => p.BirthDate, f => f.Date.Past(30, DateTime.Now.AddYears(-18)).ToUniversalTime().Date)
-        .RuleFor(p => p.Nationality, "RU")
-        .RuleFor(p => p.PassportSeries, f => f.Random.Number(1000, 9999))
-        .RuleFor(p => p.PassportNumber, f => f.Random.Number(100000, 999999))
-        .RuleFor(p => p.PhotoBase64, f => Convert.ToBase64String(f.Random.Bytes(10)))
-        .RuleFor(p => p.DateIssue, f => f.Date.Past(5).ToUniversalTime().Date);
+    public static void Main()
+    {
+        using var dbContext = new GeneratedContext();
+        // Удаление существующих записей из таблицы
+        // dbContext.Database.ExecuteSqlRaw("TRUNCATE TABLE public.\"VisitorGenerated\" RESTART IDENTITY");
+        List<string> patronymics = File.ReadAllLines("..\\..\\..\\rus_midname.txt").ToList();
+        List<string> names = File.ReadAllLines("..\\..\\..\\russian_names.txt").ToList();
+        List<string> surnames = File.ReadAllLines("..\\..\\..\\russian_surnames.txt").ToList();
 
-    // Генерация 100 записей и добавление их в базу данных
-    var visitors = faker.Generate(1000);
-    dbContext.VisitorsGenerated.AddRange(visitors);
-    dbContext.SaveChanges();
+        var faker = new Faker<Visitor>("ru")
+            .RuleFor(p => p.FirstName, f => f.PickRandom(names))
+            .RuleFor(p => p.LastName, f => f.PickRandom(surnames))
+            .RuleFor(p => p.FatherName, f => f.PickRandom(patronymics))
+            .RuleFor(p => p.City, f => f.Address.City())
+            .RuleFor(p => p.Gender, f => f.PickRandom("ЖЕН", "МУЖ"))
+            .RuleFor(p => p.BirthDate, f => f.Date.Past(30, DateTime.Now.AddYears(-18)).ToUniversalTime().Date)
+            .RuleFor(p => p.Nationality, "RU")
+            .RuleFor(p => p.PassportSeries, f => f.Random.Number(1000, 9999))
+            .RuleFor(p => p.PassportNumber, f => f.Random.Number(100000, 999999))
+            .RuleFor(p => p.PhotoBase64, "passport.jpg")
+            .RuleFor(p => p.DateIssue, f => f.Date.Past(5).ToUniversalTime().Date);
 
-    Console.WriteLine("Данные успешно добавлены в базу данных.");
-}*/
+        var visitors = new List<Visitor>();
+        int duplicateCount = 15;
 
-using Data.DAL.Entities;
+        foreach (var originalVisitor in faker.Generate(1000))
+        {
+            visitors.Add(originalVisitor);
+
+            for (int i = 0; i < duplicateCount; i++)
+            {
+                Visitor duplicateVisitor = new()
+                {
+                    FirstName = GenerateRandomNameWithError(originalVisitor.FirstName, names),
+                    LastName = GenerateRandomNameWithError(originalVisitor.LastName, surnames),
+                    FatherName = GenerateRandomNameWithError(originalVisitor.FatherName, patronymics),
+                    City = originalVisitor.City,
+                    Gender = originalVisitor.Gender,
+                    BirthDate = originalVisitor.BirthDate,
+                    Nationality = originalVisitor.Nationality,
+                    PassportSeries = GenerateRandomPassportNumber(originalVisitor.PassportSeries),
+                    PassportNumber = GenerateRandomPassportNumber(originalVisitor.PassportNumber),
+                    PhotoBase64 = originalVisitor.PhotoBase64,
+                    DateIssue = originalVisitor.DateIssue
+                };
+
+                visitors.Add(duplicateVisitor);
+            }
+        }
+
+        dbContext.VisitorsGenerated.AddRange(visitors);
+        dbContext.SaveChanges();
+
+        Console.WriteLine("Данные успешно добавлены в базу данных.");
+    }
+
+    private static string GenerateRandomNameWithError(string orig, List<string> nameList)
+    {
+        Random random = new();
+        int randomIndex = random.Next(0, nameList.Count - 1);
+        string originalName;
+        originalName = (randomIndex % 2 == 0) ? nameList[randomIndex] : orig;
+
+        char[] nameChars = originalName.ToCharArray();
+        randomIndex = random.Next(0, originalName.Length);
+        if (randomIndex % 2 == 0)
+        {
+            (nameChars[0], nameChars[randomIndex]) = (nameChars[randomIndex], nameChars[0]);
+        }
+
+        return new string(nameChars);
+    }
+
+    private static int GenerateRandomPassportNumber(int originalNumber)
+    {
+        Random random = new();
+        int randomIndex = random.Next(0, 500);
+        return (randomIndex % 2 == 0) ? originalNumber : originalNumber - originalNumber % 10;
+    }
+}
+
+
+/*using Data.DAL.Entities;
 
 string cmd = "C:/Python311/python.exe";
 string arg = "D:/учёба/qoollo-practice/backend/PolyclinicBackend/PythonService/code.py D:/passport.jpg false";
 Visitor visitor = PythonService.PythonService.GetDataFromPhoto(cmd, arg);
-Console.WriteLine(visitor.Gender);
+Console.WriteLine(visitor.Gender);*/
